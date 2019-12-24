5c6b927b7eac486e3a36370b13039114718ec48e
Yan Feng <fengyan0121@gmail.com>
2019-12-23 23:25:10 -0500

    add register

--- seekjob/jobposts/urls.py
+++ seekjob/jobposts/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='job-posts'),
-    path('about', views.about, name='about-ustc-jobseeking')
+    path('', views.home, name='jobposts-home'),
+    path('about', views.about, name='jobposts-about')
 ]
--- seekjob/jobposts/views.py
+++ seekjob/jobposts/views.py
@@ -1,23 +1,9 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'Yan Feng',
-        'title': 'Job post 1',
-        'content': 'First job post',
-        'date_posted': 'Dec 17, 20190'
-    },
-{
-        'author': 'Ru Yan',
-        'title': 'Job post 2',
-        'content': 'Second job post',
-        'date_posted': 'Dec 16, 20190'
-    }
-]
+from .models import JobPosts
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': JobPosts.objects.all()
     }
     return render(request, 'jobposts/home.html', context)
 
